db4f38263a1f38b4bb18abbbd249279c224ced9b
Mads <68258116+jomski77@users.noreply.github.com>
2025-04-21 00:44:28 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://https://picc-online-inventory.onrender.com:3000',
         secure: false,
       },
     },
